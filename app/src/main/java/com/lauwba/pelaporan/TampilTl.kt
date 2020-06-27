7858403ac38377460b49dda8ed614fa5009fb63c
Zelvia Olga Maharani <zelviaolgamaharani98@gmail.com>
2020-06-27 04:03:15 +0100

    Commit update and delete

--- app/src/main/java/com/lauwba/pelaporan/TampilTl.kt
+++ app/src/main/java/com/lauwba/pelaporan/TampilTl.kt
@@ -2,11 +2,17 @@ package com.lauwba.pelaporan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class TampilTl : AppCompatActivity() {
+class TampilTl : AppCompatActivity(), View.OnClickListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tampil_tl)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
